9f81e778a1612defb8eb118409150f0e1f4df115
Georges Gauthier <13079044+glgauthier@users.noreply.github.com>
2019-02-17 00:13:43 -0500

    turned into fxn, added NYSE and AMEX

--- getData.py
+++ getData.py
@@ -3,26 +3,34 @@
 # https://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download
 import json,csv,urllib2,os
 
-response = urllib2.urlopen('https://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download')
 
-f = open("data.csv", "w")
-f.write(response.read().replace(",\r",""))
-f.close()
+def getData(name,url):
+    if not os.path.exists("data"):
+        os.mkdir("data")
+    response = urllib2.urlopen(url)
 
-csvfile = open('data.csv', 'r')
-jsonfile = open('data.json', 'w')
-fields = ("Symbol","Name","LastSale","MarketCap","IPOyear","Sector","Industry","SummaryQuote")
-reader = csv.DictReader( csvfile, fields)
+    f = open("data/"+name+".csv", "w")
+    f.write(response.read().replace(",\r",""))
+    f.close()
 
-rows = list(reader)
-jsonfile.write("[")
-for i,row in enumerate(rows):
-    if("Sector" in row["Sector"]):
-        continue
-    
-    json.dump(row, jsonfile)
-    if(i < len(rows)-1):
-        jsonfile.write(',\n')
-    else:
-        jsonfile.write('\n]')
+    csvfile = open("data/"+name+'.csv', 'r')
+    jsonfile = open("data/"+name+'.json', 'w')
+    fields = ("Symbol","Name","LastSale","MarketCap","IPOyear","Sector","Industry","SummaryQuote")
+    reader = csv.DictReader( csvfile, fields)
+
+    rows = list(reader)
+    jsonfile.write("[")
+    for i,row in enumerate(rows):
+        if("Sector" in row["Sector"]):
+            continue
+
+        json.dump(row, jsonfile)
+        if(i < len(rows)-1):
+            jsonfile.write(',\n')
+        else:
+            jsonfile.write('\n]')
+            
+getData("NASDAQ",'https://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download')
+getData("AMEX","https://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=amex&render=download")
+getData("NYSE","https://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nyse&render=download")
     
